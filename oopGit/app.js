51d9a76f0158a959d1afd41ce396c2c6c28eca57
Jacob Gonzales <jaggtony@gmail.com>
2022-05-01 18:24:13 -0700

    addition for oop

--- oopGit/app.js
+++ oopGit/app.js
@@ -6,10 +6,13 @@
 class Dog{
     constructor(name, species, size){
         this.name=name;
-        this.species=species:
-        this.size=size:
+        this.species=species;
+        this.size=size;
     }
-
+    bark(){
+      return (this.size>60)? 'Grrrr! Grrr!' : 'Woof! Woof!'
+    }
+  }
 const fang = new Dog("Fang", "boarhound", 75);
 console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
 console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
@@ -17,7 +20,7 @@ console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
 const snowy = new Dog("Snowy", "terrier", 22);
 console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
 console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
-}
+
 
 //Character inventory
 //Improve the example RPG to add character inventory management according to the following rules:
@@ -45,22 +48,19 @@ class Character {
     attack(target) {
       if (this.health > 0) {
         const damage = this.strength;
-        console.log(
-          `${this.name} attacks ${target.name} and causes ${damage} damage points`
-          );
+        console.log(`${this.name} attacks ${target.name} and causes ${damage} damage points`);
         target.health -= damage;
         if (target.health > 0) {
           console.log(`${target.name} has ${target.health} health points left`);
         } else {
           target.health = 0;
           const bonusXP = 10;
-          this.gold+=target.gold;
-          target.gold 
-          console.log(
-            `${this
-              .name} eliminated ${target.name} and wins ${bonusXP} experience points`
-          );
+          console.log( `${this.name} eliminated ${target.name} and wins ${bonusXP} experience points`);
           this.xp += bonusXP;
+          this.gold+=target.gold;
+          this.key+=target.key;
+          target.key = 0;
+          target.gold = 0;
         }
       } else {
         console.log(`${this.name} can't attack (they've been eliminated)`);
@@ -72,3 +72,21 @@ class Character {
         .strength} as strength and ${this.xp} XP points, ${this.gold}gold and ${this.key}key(s)`;
     }
   }
+
+  const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
+
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
+
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
+
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
+
+console.log(aurora.describe());
+console.log(glacius.describe());
