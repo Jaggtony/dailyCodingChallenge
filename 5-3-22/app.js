b02d63e4fe4cbcd9430ed692a5cd558a7fafb02a
Jacob Gonzales <jaggtony@gmail.com>
2022-05-03 15:02:48 -0700

    code challenge 5-3-22

---
+++ 5-3-22/app.js
@@ -0,0 +1,5 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace=(x)=>  x.split(' ').join('')
+//split the string at the spaces to turn to 
+//an array then joing to turn array back into string but with no space in brackets so there isnt a space in the return
