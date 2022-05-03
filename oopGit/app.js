b7e1bf0019b7e206658df96408af1bbddd48d281
Jacob Gonzales <jaggtony@gmail.com>
2022-05-02 17:27:10 -0700

    oop HW

--- oopGit/app.js
+++ oopGit/app.js
@@ -90,3 +90,32 @@ glacius.attack(monster);
 
 console.log(aurora.describe());
 console.log(glacius.describe());
+
+//Account list
+//Let's build upon a previous account object exercise. A bank account is still defined by:
+//
+////A name property.
+////A balance property, initially set to 0.
+////A credit method adding the value passed as an argument to the account balance.
+////A describe method returning the account description.
+//Write a program that creates three accounts: one belonging to Sean, another to Brad and the third one to Georges. 
+//These accounts are stored in an array. 
+//Next, the program credits 1000 to each account and shows its description.
+
+class BankAcct{
+  constructor(name){
+  this.name= name;
+  this.balance = 0;
+  }
+  credit(x){
+    this.balance+=x
+  }
+  describe(){
+    console.log(`owner: ${this.name}, balance:${this.balance}`)
+  }
+}
+const arr =[new BankAcct('Sean'), new BankAcct('Brad'),new BankAcct('Georges') ]
+arr.forEach(item=>{
+  item.credit(+(1000))
+  console.log(item.describe())
+})
