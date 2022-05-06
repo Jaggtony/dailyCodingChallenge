e6d013e5f346484f073e5e6f00965daeb3247838
Jacob Gonzales <jaggtony@gmail.com>
2022-05-05 20:03:12 -0700

    code challenge 5-5-22

---
+++ 5-5-22/app.js
@@ -0,0 +1,8 @@
+//Add the value "codewars" to the websites array.
+//After your code executes the websites array should == ["codewars"]
+//
+//The websites array has already been defined for you using the following code:
+//
+//var websites = [];
+
+websites.push('codewars') //use push method to add item to array also could have use .unshift for this instance 
