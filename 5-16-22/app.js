0b3d6bdc7632359a8e2df0fb929ef6b9be361a78
Jacob Gonzales <jaggtony@gmail.com>
2022-05-16 20:26:28 -0700

    adding to an empty array

---
+++ 5-16-22/app.js
@@ -0,0 +1,14 @@
+//Add the value "codewars" to the websites array.
+//After your code executes the websites array should == ["codewars"]
+//
+//The websites array has already been defined for you using the following code:
+//
+//var websites = [];
+
+//prep
+//p:always an empty array and adding a string
+//r:no return asked for 
+//e:the contents of websites to have 'codewars
+
+websites.push('codewars') //use push method do add to the end of array, could have used unshift as well
+
