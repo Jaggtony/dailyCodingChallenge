238f9fbddb0c6671d709274d1f337517347a1ab2
Jacob Gonzales <jaggtony@gmail.com>
2022-05-24 17:38:30 -0700

    joining

---
+++ 5-24-22/app.js
@@ -0,0 +1,16 @@
+/*Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"*/
+//prep
+//p:strings and numbers
+//r:simple return
+//e:above
+function printArray(array){
+    return array.join()
+  }   //use join to make the array a string
+
+  //printArray(4,6,8,7)=>[4,6,8,7]
