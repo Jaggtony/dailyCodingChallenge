59e00d413612c3d3bbc21753be83ec894f0e6ce0
Jacob Gonzales <jaggtony@gmail.com>
2022-07-21 20:25:13 -0700

    7-21-22

---
+++ 7-20-22/app.js
@@ -0,0 +1,11 @@
+/*Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+prep
+p: given ana rray of strings, wont be empty
+r:return the array in lexicographical order
+e:['one', 'two', 'three' ]), ["one", "three", "two"]
+*/
+
+function sortme ( names ){
+    return names.sort()
+   } //use sort method on it
