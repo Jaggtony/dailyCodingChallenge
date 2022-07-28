cb7c6246ee75554ad70b4055c5febed2a6ca2695
Jacob Gonzales <jaggtony@gmail.com>
2022-07-27 21:09:11 -0700

    7-27-22

---
+++ 7-26-22/app.js
@@ -0,0 +1,15 @@
+/*rite a function that returns the number of occurrences of an element in an array.
+
+prep
+p:given an array and a number to check for matches in the array
+r:return the number of times the aarray and variable match
+e:
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+Array.prototype.numberOfOccurrences = function(y) {
+    return this.filter(x=>x== y).length
+  } //return the array after filtering to see if it matches the parameter given above 
