c07c3d524e4b82ed5952fb12d2ebe152be820b68
Jacob Gonzales <jaggtony@gmail.com>
2022-09-12 20:19:19 +0100

    9-11-22 arrays

---
+++ 9-12-22/app.js
@@ -0,0 +1,8 @@
+/* 
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+prep
+p:given an array of names, no numbers or emptys or numbers
+r:return the array in apleabetical order()
+e:['one', 'two', 'three' ]),  => ["one", "three", "two"]*/
+
+const sortme = ( names )=>  names.sort() //use sort method on the names argument
