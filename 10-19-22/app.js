7db127ea717dc84dc836890972ccea69f2e9227f
Jacob Gonzales <jaggtony@gmail.com>
2022-10-19 15:12:16 -0700

    10-19-22

---
+++ 10-19-22/app.js
@@ -0,0 +1,12 @@
+/*return the average of the goveb array rounded down to the nearest integer
+array will never be empty
+
+function getAverage(arr){
+    //loop through array to get sum
+    return Math.floor( let sum = arr.reduce((a, b)=>a+b) /arr.length)
+    //avg => sum / arr.length
+    //return
+    
+
+}*/
+const getAverage = arr => Math.floor( let sum = arr.reduce((a, b)=>a+b) /arr.length)
