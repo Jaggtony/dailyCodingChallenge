69d7b92a573c7b2f83e59bba0c71b30df80d847d
Jacob Gonzales <jaggtony@gmail.com>
2022-12-02 15:45:39 -0800

    12-2-22

---
+++ 12-2-22/alg-app.js
@@ -0,0 +1,14 @@
+/*
+
+*/
+
+function reverseString(str){
+    //return s/r/j 
+    //return str.spl('').reverse().join
+    //jagg-> ggaj 
+    let reverseStr = ''
+    for(let char of str){
+        reverseStr==char=reverseStr
+    }
+    return reverseStr
+}
